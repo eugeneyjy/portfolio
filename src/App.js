fd96ab1088202ec952cf8017bd04bd2caf0dc929
eugeneyjy <yonge@oregonstate.edu>
2022-04-27 15:07:32 -0700

    contact page layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import ContactPage from './pages/contactPage';
 import HomePage from './pages/homePage';
 import ProjectsPage from './pages/projectsPage';
 
@@ -8,6 +9,7 @@ function App() {
     <Routes>
       <Route path='/' element={<HomePage/>}/>
       <Route path='/projects' element={<ProjectsPage/>}/>
+      <Route path='/contact' element={<ContactPage/>}/>
     </Routes>
   );
 }
---
+++ src/pages/contactPage.js
@@ -0,0 +1,12 @@
+import Contact from "../components/Contact/Contact";
+import Layout from "../components/Layout/Layout";
+
+function ContactPage() {
+  return (
+    <Layout>
+        <Contact/>
+    </Layout>
+  );
+}
+
+export default ContactPage;
